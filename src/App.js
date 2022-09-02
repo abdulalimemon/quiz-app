6a87d7c9f8e654487eec4b8518318458659b2c3a
Emon02 <abdulalimemon11@gmail.com>
2022-09-03 01:00:28 +0600

    Reset password

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/SignUp';
+import ResetPassword from './components/Login/ResetPassword';
 import Nav from './components/Nav/Nav';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/resetpassword' element={<ResetPassword></ResetPassword>}></Route>
         {/* <Route path='/' element={}></Route> */}
       </Routes>
       <Footer></Footer>
--- src/components/Login/ResetPassword.js
+++ src/components/Login/ResetPassword.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import avatar from '../../assets/images/login.gif';
 
-const Login = () => {
+const ResetPassword = () => {
     return (
         <main className="2xl:min-h-screen">
             <div className="container mx-auto py-5">
-                <h1 className='my-8 font-bold text-2xl text-center md:text-left md:text-4xl md:ml-10 text-gray-700'>Login to your account</h1>
+                <h1 className='my-8 font-bold text-2xl text-center md:text-left md:text-4xl md:ml-10 text-gray-700'>Reset your password</h1>
                 <div className="gap-5 pb-10 grid sm:grid-cols-1  md:grid-cols-2">
                     <div className="flex justify-center">
                         <img className='px-5' src={avatar} alt='Sign up' />
@@ -19,23 +18,9 @@ const Login = () => {
                                 <span className="material-icons-outlined w-10 h-10 flex items-center justify-center"> alternate_email </span>
                             </div>
 
-                            <div className="w-full h-12 flex items-center border p-1 rounded-md border-slate-500 bg-white">
-                                <input className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' type="password" placeholder="Enter password" />
-                                <span className="material-icons-outlined w-10 h-10 flex items-center justify-center"> lock </span>
-                            </div>
-
-                            <div className='flex justify-end'>
-                                <Link to="/resetpassword" className='text-red-500'>Forgot password?</Link>
-                            </div>
-
                             <button className='w-full h-12 p-1'>
-                                <span className="w-full h-12 rounded-md flex items-center justify-center cursor-pointer bg-green-400  hover:bg-slate-800 font-semibold hover:text-slate-100">Login</span>
+                                <span className="w-full h-12 rounded-md flex items-center justify-center cursor-pointer bg-green-400  hover:bg-slate-800 font-semibold hover:text-slate-100">Next</span>
                             </button>
-
-                            <div>
-                                <p>Don't have an account?<Link to="/signup" className='text-red-500'> Signup</Link> instead.</p>
-                            </div>
-
                         </form>
                     </div>
                 </div>
@@ -44,4 +29,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default ResetPassword;
