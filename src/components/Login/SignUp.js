017992e63449d8db04d27e54433811852559c15b
Emon02 <abdulalimemon11@gmail.com>
2022-09-06 00:38:33 +0600

    signup user

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import github from '../../assets/images/github2.png';
+import google from '../../assets/images/google.png';
 import avatar from '../../assets/images/Sign-up.png';
+import auth from '../../firebase.init';
+
+
+
+
+
 
 const SignUp = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const [
+        createUserWithEmailAndPassword, user, loading, error,] = useCreateUserWithEmailAndPassword(auth);
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        await createUserWithEmailAndPassword(email, password);
+    }
     return (
         <main className="min-h-screen">
             <div className="container mx-auto py-5">
@@ -12,19 +36,19 @@ const SignUp = () => {
                         <img className='px-5' src={avatar} alt='Sign up' />
                     </div>
                     <div className='flex justify-center items-center'>
-                        <form className="gap-5 grid grid-cols-1 mx-auto px-5 md:m-0 md:pr-10 w-full" action="#">
+                        <form onSubmit={handleSubmit} className="gap-5 grid grid-cols-1 mx-auto px-5 md:m-0 md:pr-10 w-full" action="#">
                             <div className="w-full h-12 flex items-center border p-1 rounded-md border-slate-500 bg-white">
-                                <input type="text" className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' placeholder="Enter name" />
+                                <input ref={nameRef} type="text" className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' placeholder="Enter name" />
                                 <span className="material-icons-outlined w-10 h-10 flex items-center justify-center"> person </span>
                             </div>
 
                             <div className="w-full h-12 flex items-center border p-1 rounded-md border-slate-500 bg-white">
-                                <input className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' type="text" placeholder="Enter email" />
+                                <input ref={emailRef} className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' type="text" placeholder="Enter email" />
                                 <span className="material-icons-outlined w-10 h-10 flex items-center justify-center"> alternate_email </span>
                             </div>
 
                             <div className="w-full h-12 flex items-center border p-1 rounded-md border-slate-500 bg-white">
-                                <input className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' type="password" placeholder="Enter password" />
+                                <input ref={passwordRef} className='border-none outline-none	w-full h-full py-1 px-2 font-semibold' type="password" placeholder="Enter password" />
                                 <span className="material-icons-outlined w-10 h-10 flex items-center justify-center"> lock </span>
                             </div>
 
@@ -51,12 +75,16 @@ const SignUp = () => {
                                     <div className='w-1/2 lg:w-1/3 flex justify-center font-medium'>Or Sign up With</div>
                                     <div className='border-b-2 w-1/4 lg:w-1/3 border-slate-300'></div>
                                 </div>
-                                <div className='my-2 flex'>
-                                    <button className='w-full h-12 p-1'>
-                                        <span className="w-full h-12 rounded-md flex items-center justify-center cursor-pointer bg-green-400  hover:bg-slate-800 font-semibold hover:text-slate-100">Submit now</span>
+                                <div className='my-3 flex'>
+                                    <button className='w-full h-14 p-1'>
+                                        <span className="w-full h-14 rounded-md flex items-center justify-center cursor-pointer  hover:bg-slate-200 border-2 border-slate-600">
+                                            <img className='w-10 h-10' src={google} alt="Google" />
+                                        </span>
                                     </button>
-                                    <button className='w-full h-12 p-1'>
-                                        <span className="w-full h-12 rounded-md flex items-center justify-center cursor-pointer bg-green-400  hover:bg-slate-800 font-semibold hover:text-slate-100">Submit now</span>
+                                    <button className='w-full h-14 p-1'>
+                                        <span className="w-full h-14 rounded-md flex items-center justify-center cursor-pointer  hover:bg-slate-200 border-2 border-slate-600">
+                                            <img className='w-10 h-10' src={github} alt="Github" />
+                                        </span>
                                     </button>
                                 </div>
                             </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,10 +11,12 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_projectId,
     storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId 
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app;
+const auth = getAuth(app);
+
+export default auth;
